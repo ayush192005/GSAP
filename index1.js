70d8627991750fa93ca7217ac8e6f95fd6c58fde
ayush192005 <ayushbansal19505@gmail.com>
2026-07-10 11:19:31 +0530

    Update index1.js

--- index1.js
+++ index1.js
@@ -64,33 +64,124 @@
 // })
 
 
-var menu = document.querySelector(".nav i");
-var close = document.querySelector(".full i");
+/* ================== video 5 ================== */
+// var menu = document.querySelector(".nav i");
+// var close = document.querySelector(".full i");
 
-var nav = gsap.timeline()
+// var nav = gsap.timeline()
 
-nav.to(".full",{
-    right:0,
-    duration:0.7,
+// nav.to(".full",{
+//     right:0,
+//     duration:0.7,
+// })
+
+// nav.from(".full h4",{
+//    x:150,
+//    duration:1,
+//    stagger:0.1,
+//    opacity:0,
+// })
+
+// nav.from(".full i",{
+//     opacity:0,
+// })
+
+// nav.pause()
+
+// menu.addEventListener("click", function(){
+//     nav.play()
+// })
+
+// close.addEventListener("click",function(){
+//     nav.reverse()
+// })
+
+/* ================== video 6 ================== */
+// var h1 = document.querySelector("h1");
+// var h1Text = document.querySelector("h1").textContent
+// var h1Text = h1.textContent
+
+// var splittedText = h1Text.split("");
+
+// splittedText.forEach(function(elem){
+//     console.log(elem)
+// })
+
+// var clutter = "" 
+// splittedText.forEach(function(){
+//     clutter = clutter + "hey"
+// })
+// console.log(clutter);
+
+// var clutter = ""
+// splittedText.forEach(function(elem){
+//     clutter = clutter + elem  // clutter += elem
+// })
+// console.log(clutter);
+
+// var clutter = ""
+// splittedText.forEach(function(elem){
+//     clutter += `<span>${elem}</span>`
+// })
+
+// h1.innerHTML = clutter
+
+/* Main part  */  /* Basic level*/
+// function breakTheText(){
+//     var h1 = document.querySelector("h1");
+//     var h1Text = h1.textContent;
+//     var splittedText = h1Text.split("");
+//     var clutter = "";
+//     splittedText.forEach(function (elem) {
+//         clutter += `<span>${elem}</span>`
+//     })
+
+//     h1.innerHTML = clutter;
+// }
+
+// breakTheText()
+
+// gsap.from("h1 span",{
+//     y:70,
+//     duration:1.7,
+//     delay:0.7,
+//     opacity:0,
+//     stagger:0.15,
+// })
+
+/* Advance Level */
+function breakTheText(){
+    var h1 = document.querySelector("h1");
+    var h1Text = h1.textContent;
+    var splittedText = h1Text.split("");
+    var halfValue = splittedText.length/2;
+    var clutter = "";
+
+    splittedText.forEach(function(elem,idx){
+        if (idx<halfValue){
+            clutter += `<span class="a">${elem}</span>`
+        }else{
+              clutter += `<span class="b">${elem}</span>`
+        }
+    })
+
+    h1.innerHTML = clutter;
+}
+
+breakTheText()
+
+gsap.from("h1 .a",{
+    y:100,
+    duration:1,
+    delay:0.5,
+    stagger:0.15,
+    opactity:0,
 })
 
-nav.from(".full h4",{
-   x:150,
-   duration:1,
-   stagger:0.1,
-   opacity:0,
-})
-
-nav.from(".full i",{
-    opacity:0,
-})
-
-nav.pause()
-
-menu.addEventListener("click", function(){
-    nav.play()
-})
-
-close.addEventListener("click",function(){
-    nav.reverse()
+gsap.from("h1 .b",{
+    y:100,
+    duration:1,
+    delay:0.5,
+    stagger:-0.15,
+    opactity:0,
 })
